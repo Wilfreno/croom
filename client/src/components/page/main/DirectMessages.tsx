3ac3567a36a1e2d5d9b4dd82af6a105684195a6f
Wing <w.gayongan@gmail.com>
2024-05-19 21:04:05 +0000

    style:improve direct message ui

--- client/src/components/page/main/DirectMessages.tsx
+++ client/src/components/page/main/DirectMessages.tsx
@@ -1,8 +1,12 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { PencilSquareIcon } from "@heroicons/react/24/solid";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function DirectMessages() {
+  const { data } = useSession();
   return (
     <div className="flex flex-col space-y-5 mx-2">
       <div className="flex items-center justify-between ">
@@ -12,9 +16,25 @@ export default function DirectMessages() {
         </Button>
       </div>
       <ScrollArea className="h-[35dvh] my-2">
-        <div>
-          {Array.from({ length: 30 }).map((_, index) => (
-            <p>{index}</p>
+        <div className="mx-3 space-y-1">
+          {Array.from({ length: 30 }).map(() => (
+            <Link href={"#"}>
+              <Button
+                variant="ghost"
+                className="w-full justify-start space-x-5 rounded h-fit"
+              >
+                <Avatar className="aspect-square w-8 h-auto">
+                  <AvatarImage
+                    src={data?.user.profile_pic?.photo_url}
+                    alt={data?.user.display_name.slice(0, 1).toUpperCase()}
+                  />
+                  <AvatarFallback>
+                    {data?.user.display_name.slice(0, 1).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+                <span>{data?.user.display_name}</span>
+              </Button>
+            </Link>
           ))}
         </div>
       </ScrollArea>
--- client/src/components/svg/CompassSvg.tsx
+++ client/src/components/svg/CompassSvg.tsx
@@ -8,7 +8,7 @@ export default function CompassSvg(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 491.336 491.336"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
         strokeLinecap="round"
