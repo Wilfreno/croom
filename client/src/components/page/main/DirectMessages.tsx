8c1519c1377c36a77bb1235ccd5277aea0cf51ff
Wing <w.gayongan@gmail.com>
2024-06-03 09:27:18 +0000

    style: redesign left side bar

--- client/src/components/page/main/DirectMessages.tsx
+++ client/src/components/page/main/DirectMessages.tsx
@@ -1,19 +1,32 @@
-"use client"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+"use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { PencilSquareIcon } from "@heroicons/react/24/solid";
+import {
+  MagnifyingGlassIcon,
+  PencilSquareIcon,
+} from "@heroicons/react/24/solid";
 import { useSession } from "next-auth/react";
 
 export default function DirectMessages() {
   const { data } = useSession();
   return (
     <div className="flex flex-col space-y-5 mx-2">
-      <div className="flex items-center justify-between ">
+      <div className="flex items-center justify-between">
         <p className="font-bold text-sm">Direct messages</p>
         <Button variant="ghost" className="p-1 h-fit" type="button">
           <PencilSquareIcon className="h-4" />
         </Button>
+      </div>
+      <div className="relative my-5">
+        <Input id="search" placeholder="find conversation" autoComplete="off" />
+        <Label
+          htmlFor="search"
+          className="absolute top-1/2 right-3 -translate-y-1/2"
+        >
+          <MagnifyingGlassIcon className="h-5" />
+        </Label>
       </div>
       <ScrollArea className="h-[35dvh] my-2">
         <div className="mx-3 space-y-1"></div>
--- client/src/components/page/main/MainLeftSideBar.tsx
+++ client/src/components/page/main/MainLeftSideBar.tsx
@@ -1,4 +1,3 @@
-import SearchConversation from "./SearchConversation";
 import MainSideBarNavigation from "./MainSideBarNavigation";
 import DirectMessages from "./DirectMessages";
 import UserMenu from "./UserMenu";
@@ -6,7 +5,6 @@ import UserMenu from "./UserMenu";
 export default function MainLeftSideBar() {
   return (
     <section className="w-1/5 h-full flex flex-col border-r justify-between">
-      <SearchConversation />
       <MainSideBarNavigation />
       <DirectMessages />
       <UserMenu />
