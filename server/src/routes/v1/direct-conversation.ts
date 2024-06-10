d59120f16e0df89f5b0b1df5e3e15be896ca686a
Wilfreno Gayongan <wilfrenogayongan@gmail.com>
2024-06-10 15:47:57 +0800

    refactor: change direct conversation routes

--- server/src/routes/v1/direct-conversation.ts
+++ server/src/routes/v1/direct-conversation.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { badRequest, notFoundStatus, okStatus } from "../../lib/response-json";
+import {
+  badRequest,
+  notFoundStatus,
+  okStatus,
+  serverConflict,
+} from "../../lib/response-json";
 import { prisma } from "../../server";
+import { DirectConversation, DirectMessage } from "@prisma/client";
 
 const router = Router();
 const environment_mode = process.env.NODE_ENV;
@@ -18,36 +24,28 @@ router
         where: { id: dm_id },
       });
 
-      let message: Message;
+      let conversation: DirectConversation & { messages: DirectMessage[] };
 
       if (found_conversation) {
-        message = await prisma.message.create({
+        conversation = await prisma.directConversation.update({
+          where: {
+            id: found_conversation.id,
+          },
           data: {
-            conversation_id: found_conversation.id,
-            sender_id,
-            receiver_id,
-            text_message: {
+            messages: {
               create: {
-                content: text,
+                type: "TEXT",
+                receiver_id,
+                sender_id,
+                text_message: {
+                  create: { content: text },
+                },
               },
             },
-            type: "text",
           },
           include: {
-            text_message: true,
-          },
-        });
-        await prisma.directConversation.update({
-          where: { id: found_conversation.id },
-          data: {
             messages: {
-              connect: {
-                id: message.id,
-              },
-            },
-          },
-          select: {
-            messages: {
+              include: { text_message: true },
               orderBy: {
                 date_created: "desc",
               },
@@ -56,119 +54,46 @@ router
           },
         });
       } else {
-        const direct_conversation = await prisma.directConversation.create({
+        conversation = await prisma.directConversation.create({
           data: {
             id: dm_id,
             user1_id: sender_id,
             user2_id: receiver_id,
-          },
-        });
-
-        message = await prisma.message.create({
-          data: {
-            conversation_id: direct_conversation.id,
-            sender_id,
-            receiver_id,
-            text_message: {
+            messages: {
               create: {
-                content: text,
+                sender_id,
+                receiver_id,
+                type: "TEXT",
+                text_message: {
+                  create: {
+                    content: text,
+                  },
+                },
               },
             },
-            type: "text",
           },
           include: {
-            text_message: true,
+            messages: {
+              include: { text_message: true },
+              orderBy: {
+                date_created: "desc",
+              },
+              take: 1,
+            },
           },
         });
       }
-
-      return response.status(200).json(okStatus("message sent", message));
-    } catch (error) {
-      if (environment_mode === "development") console.error(error);
-      return response.status(400).json(badRequest());
-    }
-  })
-  .get("/", async (request, response) => {
-    try {
-      const query = request.query;
-
-      if (!query.user_id || !query.friend_id)
-        return response
-          .status(400)
-          .json(
-            badRequest(
-              "user_id and friend_id is required as a query parameter; /direct-message?user_id=&friend_id="
-            )
-          );
-
-      const user_id = query.user_id as string;
-      const friend_id = query.friend_id as string;
-
-      let count = 20;
-      let skip = 0;
-
-      if (query.page) {
-        count *= Number(query.page);
-        skip = count - 20;
-      }
-
-      const dm_id = [user_id, friend_id].sort().join("-");
-      const m = await prisma.directConversation.findFirst({
-        where: { id: dm_id },
-        select: {
-          messages: {
-            include: {
-              text_message: true,
-              photo_message: true,
-              video_message: true,
-            },
-            orderBy: {
-              date_created: "asc",
-            },
-            take: count,
-            skip,
-          },
-        },
-      });
 
       return response
         .status(200)
-        .json(okStatus("request succesfull", m!.messages));
-    } catch (error) {
-      if (environment_mode === "development") console.error(error);
-      return response.status(400).json(badRequest());
-    }
-  })
-  .delete("/", async (request, response) => {
-    try {
-      const { message_id }: Record<string, string> = request.body;
-
-      const found_message = await prisma.message.findFirst({
-        where: {
-          id: message_id,
-        },
-      });
-
-      if (!found_message)
-        return response
-          .status(409)
-          .json(
-            serverConflict("cannot delete message; message does not exist")
-          );
-
-      await prisma.message.delete({
-        where: {
-          id: message_id,
-        },
-      });
-
-      return response.status(200).json(okStatus("message deleted", null));
+        .json(okStatus("message sent", conversation.messages[0]));
     } catch (error) {
       if (environment_mode === "development") console.error(error);
       return response.status(400).json(badRequest());
     }
   })
 
+  //read routes
   .get("/:id", async (request, response) => {
     try {
       const user_id = request.params.id;
@@ -216,6 +141,88 @@ router
       return response
         .status(200)
         .json(okStatus("request successful", direct_conversation));
+    } catch (error) {
+      if (environment_mode === "development") console.error(error);
+      return response.status(400).json(badRequest());
+    }
+  })
+  .get("/messages", async (request, response) => {
+    try {
+      const query = request.query;
+
+      if (!query.user_id || !query.friend_id)
+        return response
+          .status(400)
+          .json(
+            badRequest(
+              "user_id and friend_id is required as a query parameter; /direct-message?user_id=&friend_id="
+            )
+          );
+
+      const user_id = query.user_id as string;
+      const friend_id = query.friend_id as string;
+
+      let count = 20;
+      let skip = 0;
+
+      if (query.page) {
+        count *= Number(query.page);
+        skip = count - 20;
+      }
+
+      const dm_id = [user_id, friend_id].sort().join("-");
+      const m = await prisma.directConversation.findFirst({
+        where: { id: dm_id },
+        select: {
+          messages: {
+            include: {
+              text_message: true,
+              photo_message: true,
+              video_message: true,
+            },
+            orderBy: {
+              date_created: "asc",
+            },
+            take: count,
+            skip,
+          },
+        },
+      });
+
+      return response
+        .status(200)
+        .json(okStatus("request successful", m!.messages));
+    } catch (error) {
+      if (environment_mode === "development") console.error(error);
+      return response.status(400).json(badRequest());
+    }
+  })
+
+  //delete routes
+  .delete("/", async (request, response) => {
+    try {
+      const { message_id }: Record<string, string> = request.body;
+
+      const found_message = await prisma.directMessage.findFirst({
+        where: {
+          id: message_id,
+        },
+      });
+
+      if (!found_message)
+        return response
+          .status(409)
+          .json(
+            serverConflict("cannot delete message; message does not exist")
+          );
+
+      await prisma.directMessage.delete({
+        where: {
+          id: message_id,
+        },
+      });
+
+      return response.status(200).json(okStatus("message deleted", null));
     } catch (error) {
       if (environment_mode === "development") console.error(error);
       return response.status(400).json(badRequest());
