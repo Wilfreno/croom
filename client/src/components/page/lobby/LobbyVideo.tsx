6912d883eb14eef7e257b90cea8242d68c9ec50e
Wing <w.gayongan@gmail.com>
2024-10-12 18:08:34 +0000

    chore: initialize lobby video UI

--- client/src/components/page/lobby/LobbyVideo.tsx
+++ client/src/components/page/lobby/LobbyVideo.tsx
@@ -8,6 +8,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
 import {
   Mic,
   MicOff,
@@ -21,6 +22,7 @@ import {
 import React, { useState } from "react";
 
 export default function LobbyVideo() {
+  const [videos, setVideos] = useState(["", "", "",]);
   const [status, setStatus] = useState({
     volume: 0,
     audio: false,
@@ -28,111 +30,120 @@ export default function LobbyVideo() {
   });
 
   return (
-    <section className="flex flex-col w-full h-full max-h-dvh bg-secondary items-center justify-center">
-      <div className="aspect-video w-full h-auto max-w-[80vw] bg-secondary-foreground relative">
-        <div className="absolute bottom-5 left-1/2 -translate-x-1/2 gap-12  flex">
-          <div className="flex items-center gap-2">
-            <span className="h-6">
-              {status.volume ? (
-                status.volume > 80 ? (
-                  <Volume2 className="h-full w-auto stroke-primary" />
-                ) : (
-                  <Volume1 className="h-full w-auto stroke-primary" />
-                )
+    <section className="w-full h-full max-h-dvh bg-secondary-foreground relative">
+      <div className="w-full h-full flex flex-wrap items-center justify-center p-1 gap-1">
+        {videos.slice(0, 4).map((_, index) => (
+          <div
+            key={index}
+            className="h-1/2 w-[49.5%]   bg-red-500 flex items-center justify-center "
+          >
+            <span className="text-xl font-bold">{index}</span>
+          </div>
+        ))}
+        
+      </div>
+      <div className="gap-12 flex absolute bottom-4 left-1/2 -translate-x-1/2">
+        <div className="flex items-center gap-2">
+          <span className="h-6">
+            {status.volume ? (
+              status.volume > 80 ? (
+                <Volume2 className="h-full w-auto stroke-primary" />
               ) : (
-                <VolumeX className="h-full w-auto stroke-primary" />
-              )}
-            </span>
-            <div>
-              <Input
-                className="w-12 h-auto text-xs bg-primary text-center"
-                type="text"
-                inputMode="numeric"
-                id="custom"
-                max={100}
-                min={0}
-                value={status.volume.toString()}
-                onChange={(e) => {
-                  if (!e.target.value) {
-                    setStatus((prev) => ({
-                      ...prev,
-                      volume: 0,
-                    }));
-                    return;
-                  }
-                  if (!Number(e.target.value)) return;
-                  if (Number(e.target.value) < 0) {
-                    setStatus((prev) => ({
-                      ...prev,
-                      volume: 0,
-                    }));
-                    return;
-                  }
-                  if (Number(e.target.value) > 100) {
-                    setStatus((prev) => ({
-                      ...prev,
-                      volume: 100,
-                    }));
-                    return;
-                  }
-
+                <Volume1 className="h-full w-auto stroke-primary" />
+              )
+            ) : (
+              <VolumeX className="h-full w-auto stroke-primary" />
+            )}
+          </span>
+          <div>
+            <Input
+              className="w-12 h-auto text-xs bg-primary text-center"
+              type="text"
+              inputMode="numeric"
+              id="custom"
+              max={100}
+              min={0}
+              value={status.volume.toString()}
+              onChange={(e) => {
+                if (!e.target.value) {
                   setStatus((prev) => ({
                     ...prev,
-                    volume: Number(e.target.value),
+                    volume: 0,
                   }));
-                }}
-              />
-            </div>
-            <TooltipProvider disableHoverableContent delayDuration={0}>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Slider
-                    className="w-28"
-                    value={[status.volume]}
-                    max={100}
-                    step={1}
-                    onValueChange={(e) =>
-                      setStatus((prev) => ({ ...prev, volume: e[0] }))
-                    }
-                  />
-                </TooltipTrigger>
-                <TooltipContent>{status.volume}</TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
+                  return;
+                }
+                if (!Number(e.target.value)) return;
+                if (Number(e.target.value) < 0) {
+                  setStatus((prev) => ({
+                    ...prev,
+                    volume: 0,
+                  }));
+                  return;
+                }
+                if (Number(e.target.value) > 100) {
+                  setStatus((prev) => ({
+                    ...prev,
+                    volume: 100,
+                  }));
+                  return;
+                }
+
+                setStatus((prev) => ({
+                  ...prev,
+                  volume: Number(e.target.value),
+                }));
+              }}
+            />
           </div>
-          <div className="flex gap-4">
-            <Button
-              size="icon"
-              onClick={() =>
-                setStatus((prev) => ({ ...prev, audio: !prev.audio }))
-              }
-            >
-              <span className="h-4">
-                {status.audio ? (
-                  <MicOff className="h-full w-auto" />
-                ) : (
-                  <Mic className="h-full w-auto" />
-                )}
-              </span>
-            </Button>
-            <Button
-              size="icon"
-              onClick={() =>
-                setStatus((prev) => ({ ...prev, video: !prev.video }))
-              }
-            >
-              <span className="h-4">
-                {status.video ? (
-                  <VideoOff className="h-full w-auto" />
-                ) : (
-                  <Video className="h-full w-auto" />
-                )}
-              </span>
-            </Button>
-            <Button size="icon" variant="destructive">
-              <PhoneOff className="h-4 w-auto" />
-            </Button>
-          </div>
+          <TooltipProvider disableHoverableContent delayDuration={0}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Slider
+                  className="w-28"
+                  value={[status.volume]}
+                  max={100}
+                  step={1}
+                  onValueChange={(e) =>
+                    setStatus((prev) => ({ ...prev, volume: e[0] }))
+                  }
+                />
+              </TooltipTrigger>
+              <TooltipContent>{status.volume}</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+        <div className="flex gap-4">
+          <Button
+            size="icon"
+            onClick={() =>
+              setStatus((prev) => ({ ...prev, audio: !prev.audio }))
+            }
+          >
+            <span className="h-4">
+              {status.audio ? (
+                <MicOff className="h-full w-auto" />
+              ) : (
+                <Mic className="h-full w-auto" />
+              )}
+            </span>
+          </Button>
+          <Button
+            size="icon"
+            onClick={() =>
+              setStatus((prev) => ({ ...prev, video: !prev.video }))
+            }
+          >
+            <span className="h-4">
+              {status.video ? (
+                <VideoOff className="h-full w-auto" />
+              ) : (
+                <Video className="h-full w-auto" />
+              )}
+            </span>
+          </Button>
+          <Button size="icon" variant="destructive">
+            <PhoneOff className="h-4 w-auto" />
+          </Button>
         </div>
       </div>
     </section>
