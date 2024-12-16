8fe390f2feaef01ea2ac73de84d16b923b92a90e
Wilfreno Gayongan <w.gayongan@gmail.com>
2024-12-17 01:57:35 +0800

    feature: added active friends section

--- client/src/components/page/main/home/HomeActiveFriends.tsx
+++ client/src/components/page/main/home/HomeActiveFriends.tsx
@@ -1,15 +1,42 @@
+"use client";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import UserAvatar from "../UserAvatar";
+import { useQuery } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+import { toast } from "sonner";
+import { GETRequest } from "@/lib/server/requests";
 
 export default function HomeActiveFriends() {
+  const { data: session } = useSession();
+
+  const { data: active_friends } = useQuery({
+    enabled: !!session,
+    queryKey: [session, "active friends"],
+    queryFn: async () => {
+      try {
+        const { data, status, message } = await GETRequest<{ name: string; url?: string }[]>("/v1/user/active-friends");
+
+        if (status !== "OK") throw new Error(message);
+        return data;
+      } catch (error) {
+        toast.error((error as Error).message);
+        throw error;
+      }
+    },
+    placeholderData: [],
+  });
+
   return (
-    <section className="w-full grid">
+    <section className="w-full flex flex-col gap-1">
       <p className="font-bold">Active friends</p>
-      <div className="flex items-center gap-2">
-        <ScrollArea className="w-72 py-2 pb-3  h-16">
+      <div className="flex items-center gap-2 mx-auto">
+        <ScrollArea className="w-80 py-2 pb-3  h-fit">
           <div className="flex items-center gap-2">
-            {Array.from({ length: 10 }).map((_, index) => (
-              <UserAvatar key={index} />
+            {active_friends!.map((active_friend) => (
+              <div key={active_friend.name} className="flex flex-col items-center gap-1">
+                <UserAvatar is_online src={active_friend.url} />
+                <span className="text-xs font-medium">{active_friend.name}</span>
+              </div>
             ))}
           </div>
           <ScrollBar orientation="horizontal" />
