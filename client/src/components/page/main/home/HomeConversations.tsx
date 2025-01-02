77a4022bb1d00fab11420b3dd40fd49f7d0cb1a4
Wilfreno Gayongan <w.gayongan@gmail.com>
2025-01-02 10:26:38 +0800

    fix: conversation list does not display of the conversation is online

--- client/src/components/page/main/home/HomeConversations.tsx
+++ client/src/components/page/main/home/HomeConversations.tsx
@@ -2,22 +2,20 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import SearchConversation from "./SearchConversation";
 import { Button } from "@/components/ui/button";
-import { SquarePen, UserRound } from "lucide-react";
+import { SquarePen } from "lucide-react";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import { toast } from "sonner";
 import { GETRequest } from "@/lib/server/requests";
-import { Conversation, User } from "@/lib/types/server-data-types";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { cn } from "@/lib/utils";
+import { Conversation } from "@/lib/types/server-data-types";
 import { useMemo } from "react";
+import HomeConversation from "./HomeConversation";
 
 export default function HomeConversations() {
   const router = useRouter();
   const { data: session } = useSession();
-  const pathname = usePathname();
 
   const { data: conversations } = useQuery({
     enabled: !!session,
@@ -66,78 +64,9 @@ export default function HomeConversations() {
       <SearchConversation />
       <ScrollArea className="h-[65dvh]">
         <div className="grid gap-1">
-          {to_display_conversation.map((convo) => {
-            let conversation_name = convo.name;
-            let other_user: User;
-            let photo_url: string | undefined = convo.photo?.url;
-            let seen = convo.messages[0]?.sender.id === session?.user.id;
-
-            if (!seen && convo.messages[0]?.seen_by.some((user) => user.id === session?.user.id)) seen = true;
-
-            if (!convo.is_group_chat) {
-              other_user = convo.members.find((member) => member.id !== session?.user.id)!;
-              conversation_name = other_user.display_name;
-              photo_url = other_user.photo?.url;
-            }
-
-            const date_sent = new Date(convo.messages[0].date_created);
-            const now = new Date();
-            const relative_date_in_seconds = Math.floor((now.getTime() - date_sent.getTime()) / 1000);
-
-            const day = 60 * 60 * 24;
-
-            let time_interval_text = "";
-
-            if (relative_date_in_seconds < day) {
-              time_interval_text +=
-                " " +
-                new Intl.DateTimeFormat("en-US", {
-                  minute: "2-digit",
-                  hour: "2-digit",
-                }).format(date_sent);
-            } else {
-              time_interval_text += Math.floor(relative_date_in_seconds / day) + " d";
-            }
-            return (
-              <div
-                key={convo.id}
-                className={cn(
-                  "flex items-center justify-start gap-2 w-full h-fit p-2  rounded-sm relative hover:bg-muted cursor-pointer",
-                  pathname.startsWith("/conversation/" + convo.id) && "bg-muted"
-                )}
-                onClick={() => router.push("/conversation/" + convo.id)}
-              >
-                {!seen && (
-                  <span className="absolute top-1 right-1 aspect-square h-4 w-auto bg-primary rounded-full"></span>
-                )}
-                <Avatar>
-                  <AvatarImage src={photo_url} />
-                  <AvatarFallback>
-                    <UserRound className="h-1/2 w-auto" />
-                  </AvatarFallback>
-                </Avatar>
-                <div className="flex flex-col items-start justify-start w-full">
-                  <span className="font-semibold truncate max-w-60">{conversation_name}</span>
-                  <div
-                    className={cn(
-                      "flex items-center justify-between text-xs w-full",
-                      seen ? "text-muted-foreground" : "font-semibold"
-                    )}
-                  >
-                    <p className="truncate  max-w-48">
-                      {convo.messages[0]?.sender.id === session?.user.id && <span>you: </span>}
-                      <span>
-                        {convo.messages[0].text
-                          ? convo.messages[0].text
-                          : convo.messages[0].sender.id + " sent a photo"}
-                      </span>
-                    </p>
-                    <p>{time_interval_text}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+          {to_display_conversation.map((convo) => (
+            <HomeConversation key={convo.id} convo={convo} />
+          ))}
         </div>
       </ScrollArea>
     </section>
