402599bf00beeeaabb0e79043820c73eae9631a0
Wilfreno Gayongan <w.gayongan@gmail.com>
2025-01-02 23:48:35 +0800

    fix: nickname UI and remove input value when closing the dialog

--- client/src/components/page/main/info/customization/AddNickname.tsx
+++ client/src/components/page/main/info/customization/AddNickname.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { getConvoOptions } from "@/lib/react-query/prefetch-query-options";
 import { Conversation } from "@/lib/types/server-data-types";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { CaseSensitive, Check, PenLine } from "lucide-react";
+import { CaseSensitive, Check, PenLine, X } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import UserAvatar from "../../UserAvatar";
@@ -15,7 +15,7 @@ import { PATCHRequest } from "@/lib/server/requests";
 
 export default function AddNickname() {
   const [nicknames, setNickname] = useState<Conversation["nicknames"]>([]);
-  const [open, setOpen] = useState("");
+  const [selected, setSelected] = useState("");
   const [new_nickname, setNewNickname] = useState<{ user: string; value: string }>({ user: "", value: "" });
 
   const { data: session } = useSession();
@@ -27,7 +27,7 @@ export default function AddNickname() {
     mutationFn: async () => {
       try {
         const { status, message } = await PATCHRequest("/v1/conversation/" + params.id + "/nicknames", {
-          nicknames: new_nickname,
+          nickname: new_nickname,
         });
 
         if (status !== "OK") throw new Error(message);
@@ -45,22 +45,39 @@ export default function AddNickname() {
           nicknames: prev.nicknames.map((nickname) => (nickname.user === new_nickname.user ? new_nickname : nickname)),
         };
       });
+      query_client.setQueryData<Conversation[]>([session?.user.id, "conversations"], (prev) => {
+        if (!prev) return;
+        return prev.map((conversation) =>
+          conversation.id === params.id
+            ? {
+                ...conversation,
+                nicknames: conversation.nicknames.map((nickname) =>
+                  nickname.user === new_nickname.user ? new_nickname : nickname
+                ),
+              }
+            : conversation
+        );
+      });
       setNewNickname({ user: "", value: "" });
+      setSelected("");
     },
   });
 
   useEffect(() => {
     if (!conversation) return;
 
-    if (!conversation.nicknames.length) {
-      setNickname(conversation.members.map((user) => ({ user: user.id, value: "" })));
-    } else {
-      setNickname(conversation.nicknames);
-    }
+    setNickname(conversation.nicknames);
   }, [conversation]);
 
   return (
-    <Dialog>
+    <Dialog
+      onOpenChange={(is_open) => {
+        if (!is_open) {
+          setNewNickname({ user: "", value: "" });
+          setSelected("");
+        }
+      }}
+    >
       <DialogTrigger asChild>
         <Button variant="ghost" className="w-full justify-start">
           <span className="aspect-square h-fit w-auto bg-secondary p-2 rounded-full">
@@ -73,33 +90,76 @@ export default function AddNickname() {
         <DialogHeader>
           <DialogTitle>Add Nicknames</DialogTitle>
         </DialogHeader>
+        <DialogClose className="absolute top-4 right-2 aspect-square h-fit w-auto p-2 rounded-full hover:bg-secondary">
+          <X className="h-4 w-auto" />
+        </DialogClose>
         <div className="grid space-y-2">
           {nicknames.map(({ user: id, value: nickname }) => {
-            return open === id ? (
+            return selected === id ? (
               <form
                 key={id}
-                className="flex items-center gap-2 h-fit w-full p-2"
+                className="flex items-start gap-2 h-fit w-full p-2"
                 autoComplete="off"
                 onSubmit={(e) => {
                   e.preventDefault();
                   set_nickname.mutate();
                 }}
               >
-                <Input
-                  autoFocus
-                  placeholder="Set nickname"
-                  onBlur={() => {
-                    if (open === id && !new_nickname.value) setOpen("");
-                  }}
-                  value={new_nickname.value}
-                  onChange={(e) => setNewNickname({ user: id, value: e.currentTarget.value })}
-                />
+                <div className="flex items-center gap-2 w-full">
+                  <UserAvatar
+                    src={
+                      id === session?.user.id
+                        ? session.user.photo?.url
+                        : conversation?.members.find((member) => member.id === id)?.photo?.url
+                    }
+                    is_online={
+                      id === session?.user.id
+                        ? false
+                        : conversation?.members.find((member) => member.id === id)?.status === "ONLINE"
+                    }
+                  />
+                  <div className="grid w-full gap-1">
+                    <div className="relative">
+                      <Input
+                        autoFocus
+                        placeholder="Set nickname"
+                        className="w-full"
+                        onBlur={() => {
+                          if (selected === id && !new_nickname.value) setSelected("");
+                        }}
+                        value={new_nickname.value}
+                        onChange={(e) => setNewNickname({ user: id, value: e.currentTarget.value })}
+                      />
+                      {!!new_nickname.value && (
+                        <Button
+                          type="button"
+                          variant="outline"
+                          className="aspect-square h-fit w-auto p-1 rounded-full absolute top-1/2 right-2 -translate-y-1/2"
+                          onClick={() => setNewNickname((prev) => ({ ...prev, value: "" }))}
+                        >
+                          <X className="h-4 w-auto" />
+                        </Button>
+                      )}
+                    </div>
+                    <span className="text-xs italic">
+                      {conversation?.members.find((member) => member.id === id)?.display_name}
+                    </span>
+                  </div>
+                </div>
                 <Button type="submit" variant="secondary" className="aspect-square h-fit w-auto p-2">
                   <Check className="h-4 w-auto text-green-500" />
                 </Button>
               </form>
             ) : (
-              <Button variant="ghost" key={id} className="justify-between h-fit w-full p-2" onClick={() => setOpen(id)}>
+              <Button
+                variant="ghost"
+                key={id}
+                className="justify-between h-fit w-full p-2"
+                onClick={() => {
+                  setSelected(id);
+                  setNewNickname({ user: id, value: nickname });
+                }}
+              >
                 <div className="flex items-center gap-2">
                   <UserAvatar
                     src={
@@ -113,15 +173,15 @@ export default function AddNickname() {
                         : conversation?.members.find((member) => member.id === id)?.status === "ONLINE"
                     }
                   />
-                  <div className="grid gap-2 ">
-                    <span className="font-medium">
+                  <div className="grid text-start">
+                    {nickname ? (
+                      <span className="font-bold text-base">{nickname}</span>
+                    ) : (
+                      <span className="italic text-muted-foreground w-fit">Set nickname</span>
+                    )}
+                    <span className="text-xs italic font-medium">
                       {conversation?.members.find((member) => member.id === id)?.display_name}
                     </span>
-                    {nickname ? (
-                      <span>{nickname}</span>
-                    ) : (
-                      <span className="text-xs italic text-muted-foreground w-fit">Set nickname</span>
-                    )}
                   </div>
                 </div>
                 <span className="aspect-square h-fit w-auto p-2 bg-primary rounded-full">
