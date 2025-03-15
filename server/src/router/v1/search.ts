666a571636d66bc23417778f8797feb1679463f5
Wilfreno Gayongan <w.gayongan@gmail.com>
2025-03-15 18:34:46 +0800

    fix: deployment of files to ec2 instance

--- server/src/router/v1/search.ts
+++ server/src/router/v1/search.ts
@@ -4,77 +4,66 @@ import User, { UserSchema } from "../../database/models/User";
 import JSONResponse from "../../lib/json-response";
 import { preValidation } from "../../lib/middleware";
 
-export default function v1SearchRouter(
-  fastify: FastifyInstance,
-  _: FastifyPluginOptions,
-  done: () => void
-) {
+export default function v1SearchRouter(fastify: FastifyInstance, _: FastifyPluginOptions, done: () => void) {
   //create
   //read
 
-  fastify.get<{ Querystring: { value: string } }>(
-    "/",
-    { preValidation: preValidation },
-    async (request, reply) => {
-      try {
-        const { value } = request.query;
-        const user = request.user as UserSchema & { id: string };
-        if (!value)
-          return reply
-            .code(400)
-            .send(JSONResponse("BAD_REQUEST", "search is required as a search query"));
+  fastify.get<{ Querystring: { value: string } }>("/", { preValidation: preValidation }, async (request, reply) => {
+    try {
+      const { value } = request.query;
+      const user = request.user as UserSchema & { id: string };
+      if (!value) return reply.code(400).send(JSONResponse("BAD_REQUEST", "search is required as a search query"));
 
-        const found_users = await User.find({
-          $and: [
-            {
-              $or: [
-                {
-                  username: {
-                    $regex: "^" + value,
-                    $options: "i",
-                  },
+      const found_users = await User.find({
+        $and: [
+          {
+            $or: [
+              {
+                username: {
+                  $regex: "^" + value,
+                  $options: "i",
                 },
-                {
-                  display_name: {
-                    $regex: "^" + value,
-                    $options: "i",
-                  },
+              },
+              {
+                display_name: {
+                  $regex: "^" + value,
+                  $options: "i",
                 },
-              ],
-            },
-            {
-              _id: { $ne: user.id },
-            },
-          ],
-        })
-          .select("display_name username photo status")
-          .populate({ path: "photo", select: "url" });
-
-        const found_chat_rooms = await Conversation.find({
-          name: {
-            $regex: "^" + value,
-            $options: "i",
+              },
+            ],
           },
-        })
-          .select("photo members")
-          .populate({ path: "photo", select: "url" })
-          .populate({ path: "member", select: "status" });
+          {
+            _id: { $ne: user.id },
+          },
+        ],
+      })
+        .select("display_name username photo status")
+        .populate({ path: "photo", select: "url" });
 
-        return reply.code(200).send(
-          JSONResponse("OK", "request successful", [
-            ...found_users.map((user) => ({ ...user.toJSON(), type: "USER" })),
-            ...found_chat_rooms.map((found_chat_room) => ({
-              ...found_chat_room.toJSON(),
-              type: "CHAT_ROOM",
-            })),
-          ])
-        );
-      } catch (error) {
-        fastify.log.error(error);
-        return reply.code(500).send(JSONResponse("INTERNAL_SERVER_ERROR"));
-      }
+      const found_chat_rooms = await Conversation.find({
+        name: {
+          $regex: "^" + value,
+          $options: "i",
+        },
+      })
+        .select("photo members")
+        .populate({ path: "photo", select: "url" })
+        .populate({ path: "member", select: "status" });
+
+      return reply.code(200).send(
+        JSONResponse("OK", "request successful", [
+          ...found_users.map((user) => ({ ...user.toJSON(), type: "USER" })),
+          ...found_chat_rooms.map((found_chat_room) => ({
+            ...found_chat_room.toJSON(),
+            type: "CHAT_ROOM",
+          })),
+        ])
+      );
+    } catch (error) {
+      fastify.log.error(error);
+      return reply.code(500).send(JSONResponse("INTERNAL_SERVER_ERROR"));
     }
-  );
+  });
   //update
   //delete
 
